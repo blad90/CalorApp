eedb92ae98a2239de4462570d82a67617455af5c
Bladimir Baez <bladimirbaezt@gmail.com>
2017-05-05 15:44:42 -0400

    Initial Commit

---
+++ Calorapp - Calories Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Calorapp - Calories Tracker
+//
+//  Created by Bladimir Baez on 5/5/17.
+//  Copyright © 2017 Apple Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
